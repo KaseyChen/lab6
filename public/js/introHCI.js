5d0b97bfddf4275c5f9571c11c7523e2983c1017
Kasey Chen <ksc003@ucsd.edu>
2021-02-11 10:47:12 -0800

    completed lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var URL = "/project/" + idNumber;
+	$.get(URL, callBackFn);
+}
+
+
+function callBackFn(result) {
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' +
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] +
+		'</small>' + result['summary'] + '</p></a>';
+	$("#project" + result['id'] + " .details").html(projectHTML);
 }
